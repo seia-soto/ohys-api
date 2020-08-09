d85f0f7eb5727a8a9604b1239c756a604f28bcfe
Seia-Soto <seia@outlook.kr>
2020-08-09 13:26:30 +0900

    Create method to search metadata

--- anilist/index.js
+++ anilist/index.js
@@ -1,2 +1,3 @@
 module.exports.debug = require('./debug')
 module.exports.queryGQL = require('./queryGQL')
+module.exports.searchMetadata = require('./searchMetadata')
---
+++ anilist/searchMetadata.js
@@ -0,0 +1,53 @@
+const debug = require('./debug')
+const queryGQL = require('./queryGQL')
+
+module.exports = async title => {
+  debug('searching anime metadata on anilist api: ' + title)
+
+  const query = `
+query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+  Page (page: $page, perPage: $perPage) {
+    media (id: $id, search: $search) {
+      id
+      idMal
+      startDate {
+        year
+        month
+        day
+      }
+      endDate {
+        year
+        month
+        day
+      }
+      season
+      seasonYear
+      type
+      format
+      status
+      episodes
+      duration
+      chapters
+      volumes
+      isAdult
+      genres
+      source
+      title {
+        romaji
+        english
+        native
+      }
+    }
+  }
+}
+  `
+  const variables = {
+    search: title,
+    page: 1,
+    perPage: 1
+  }
+
+  const result = await queryGQL(query, JSON.stringify(variables))
+
+  return result
+}
