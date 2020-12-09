eb07c861b77ad8f28a35ae0f3238806a06f43b6a
Seia-Soto <seia@outlook.kr>
2020-12-09 23:33:23 +0900

    Update database schema

--- structures/database/tables/anime.js
+++ structures/database/tables/anime.js
@@ -5,7 +5,10 @@ module.exports = table => {
   table.string('title', 1024)
   table.string('broadcaster', 512)
   table.string('status', 64)
-  table.date('releaseDate')
+  table.date('release')
+  table.string('genres', 2048) // NOTE: encoder=base64; divider=semicolon;
+  table.integer('airingDay')
+  table.time('airingTime')
 
   return table
 }
--- structures/database/tables/schedule.js
+++ structures/database/tables/schedule.js
@@ -5,7 +5,7 @@ module.exports = table => {
   // NOTE: metadata;
   table.integer('year')
   table.integer('quarter')
-  table.integer('day') // NOTE: starting from 0;
+  table.integer('day') // NOTE: starting from 0; also duplicate the value of `anime` for fast search;
   table.string('comment', 2048)
 
   return table
