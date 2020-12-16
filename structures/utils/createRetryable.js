55794d4cd2b47b1ac0dd190a4ac7b9ebdb037284
Seia-Soto <seia@outlook.kr>
2020-12-17 04:00:11 +0900

    Create retryable function to handle errors in future

---
+++ structures/utils/createRetryable.js
@@ -0,0 +1,35 @@
+const createLogger = require('./createLogger')
+
+const debug = createLogger('utils/retryable')
+
+const retryable = async (task, opts = {}) => {
+  opts.maxTries = opts.maxTries || 5
+
+  let retries = 0
+
+  try {
+    const result = await task()
+
+    return {
+      status: true,
+      resposne: result
+    }
+  } catch (error) {
+    debug('unknown error occured while running retryable:', error)
+
+    if (error) {
+      retries++
+
+      if (retries >= opts.maxTries) {
+        return retryable()
+      } else {
+        return {
+          stauts: false,
+          response: error
+        }
+      }
+    }
+  }
+}
+
+module.exports = retryable
--- structures/utils/index.js
+++ structures/utils/index.js
@@ -1,2 +1,3 @@
 module.exports.createCron = require('./createCron')
 module.exports.createLogger = require('./createLogger')
+module.exports.createRetryable = require('./createRetryable')
