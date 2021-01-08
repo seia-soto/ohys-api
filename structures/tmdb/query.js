2ebdcdfddfc5d7e87c33ace5e0ef11f8baaecc23
Seia-Soto <seia@outlook.kr>
2021-01-09 05:47:05 +0900

    Make API request non-failable

--- structures/tmdb/query.js
+++ structures/tmdb/query.js
@@ -4,8 +4,10 @@ const config = require('../../config')
 const { name, version } = require('../../package.json')
 const debug = require('./debug')
 
-module.exports = async (path, opts) => {
+const query = async (path, opts, retries) => {
   'use strict'
+
+  if (retries >= 5) return false
 
   opts = opts || {}
   opts.api_key = opts.api_key || config.externals.tmdb.v3ApiKey
@@ -14,12 +16,18 @@ module.exports = async (path, opts) => {
 
   debug('requesting to:', url.replace(opts.api_key, ''))
 
-  const res = await fetch(url, {
-    headers: {
-      'User-Agent': `Seia-Soto/${name} v${version} (https://github.com/Seia-Soto)`
-    }
-  })
-  const data = await res.json()
+  try {
+    const res = await fetch(url, {
+      headers: {
+        'User-Agent': `Seia-Soto/${name} v${version} (https://github.com/Seia-Soto)`
+      }
+    })
+    const data = await res.json()
 
-  return data
+    return data
+  } catch (e) {
+    return query(path, opts, retries + 1)
+  }
 }
+
+module.exports = query
