f42853a9a6e656eb7a64a8e1ec42190ba5000dce
Seia-Soto <seia@outlook.kr>
2021-01-12 16:10:32 +0900

    cover the error

--- structures/tasks/updateFeed.js
+++ structures/tasks/updateFeed.js
@@ -24,58 +24,62 @@ module.exports = async () => {
 
   debug('updating feed')
 
-  // NOTE: get missing items;
-  const [lastItem] = await knex('episode')
-    .select('hash')
-    .orderBy('id', 'desc')
-    .limit(1)
-  const lastHash = (lastItem || {}).hash
-  const missings = []
+  try {
+    // NOTE: get missing items;
+    const [lastItem] = await knex('episode')
+      .select('hash')
+      .orderBy('id', 'desc')
+      .limit(1)
+    const lastHash = (lastItem || {}).hash
+    const missings = []
 
-  for (let page = 0, feed = []; (feed = await ohys.getFeed({ page, prettify: 1 })).length; page++) {
-    for (let i = 0, l = feed.length; i < l; i++) {
-      feed[i].hash = sha256(feed[i].name)
+    for (let page = 0, feed = []; (feed = await ohys.getFeed({ page, prettify: 1 })).length; page++) {
+      for (let i = 0, l = feed.length; i < l; i++) {
+        feed[i].hash = sha256(feed[i].name)
 
-      if (feed[i].hash === lastHash) {
-        page = -1
+        if (feed[i].hash === lastHash) {
+          page = -1
 
-        break
+          break
+        }
+
+        missings.push(feed[i])
       }
 
-      missings.push(feed[i])
+      if (page < 0) break
     }
 
-    if (page < 0) break
+    // NOTE: normalize the data list to object by series name;
+    const animeIds = {}
+    const insertions = []
+
+    for (let i = 0, l = missings.length; i < l; i++) {
+      const item = missings[l - i - 1]
+      const data = ohys.parseTitle(item.name)
+
+      animeIds[data.series] = animeIds[data.series] || await updateAnimeOf({ name: data.series })
+
+      insertions.push({
+        updatedAt: knex.fn.now(),
+        animeId: animeIds[data.series],
+        number: data.episode,
+        resolution: data.resolution,
+        filename: normalizeFilename(item.name),
+        hash: item.hash
+      })
+    }
+
+    debug('inserting episodes missing on database:', insertions.length)
+
+    for (let i = 0, l = insertions.length; i < l; i += 5) {
+      await knex('episode')
+        .insert(insertions.slice(i, i + 5))
+    }
+
+    debug('updated feed')
+  } catch (error) {
+    debug('error while updating feed:', error)
   }
-
-  // NOTE: normalize the data list to object by series name;
-  const animeIds = {}
-  const insertions = []
-
-  for (let i = 0, l = missings.length; i < l; i++) {
-    const item = missings[l - i - 1]
-    const data = ohys.parseTitle(item.name)
-
-    animeIds[data.series] = animeIds[data.series] || await updateAnimeOf({ name: data.series })
-
-    insertions.push({
-      updatedAt: knex.fn.now(),
-      animeId: animeIds[data.series],
-      number: data.episode,
-      resolution: data.resolution,
-      filename: normalizeFilename(item.name),
-      hash: item.hash
-    })
-  }
-
-  debug('inserting episodes missing on database:', insertions.length)
-
-  for (let i = 0, l = insertions.length; i < l; i += 5) {
-    await knex('episode')
-      .insert(insertions.slice(i, i + 5))
-  }
-
-  debug('updated feed')
 
   isTaskActive = 0
 }
